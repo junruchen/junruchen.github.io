b20fa33e25ef04ea74a8a801647d282f52ca4a01
陈俊汝 <chenjunru05@gmail.com>
2018-04-12 18:02:26 +0800

    更新eventBus事件

--- dailyPractice/business/eventBus.js
+++ dailyPractice/business/eventBus.js
@@ -1,8 +1,6 @@
 /**
  * 实现一个event bus
  *
- * ctx ------ 不懂
- *
  * apis(name参数是字符串，可以同时处理多个事件名，空格隔开):
  *
  * - on(name, cb, ctx)
@@ -32,69 +30,101 @@ class Event {
   }
 
   on (name, cb, ctx) {
-    if (!name && typeof name !== 'string') {
-      console.log('msg-----不合法')
-      return false
-    }
-    if (!cb) {
-      console.log('msg-----未指定回调函数')
-      return false
-    }
-    if (!cb.name) {
-      console.log('msg-----传入的回调函数为匿名函数，无法删除')
-    }
-    let names = name.split(' ')
-    names.forEach((n) => {
-      if (!this._events[n]) {
-        this._events[n] = []
-      }
-      this._events[n].push(cb)
-    })
+    bindeEvent(this, name, cb, ctx, false)
   }
 
-  once () {
+  once (name, cb, ctx) {
+    bindeEvent(this, name, cb, ctx, true)
   }
 
   off (name, cb, ctx) {
-    let cbs = this._events[name]
-    if (!cbs) {
-      return false
+    if (typeof name !== 'string' || typeof cb !== 'function') {
+      console.log('msg-----不合法')
+      return
     }
-    if (!cb) {
-      this._events[name] = null
-    } else {
-      cbs.forEach((item, idx) => {
-        if (cb === item) {
-          cbs.splice(idx, 1)
+    if (!name) {
+      this._events = {}
+      console.log('msg-----删除全部')
+      return
+    }
+    let names = name.split(' ')
+    names.forEach((n) => {
+      if (this._events[n]) {
+        if (!cb) {
+          this._events[n] = null
+          console.log('msg-----删除该 ' + n + ' 下所有方法')
+        } else {
+          let cbs = this._events[n]
+          cbs.forEach((h, idx) => {
+            if (h.cb === cb) {
+              cbs.splice(idx, 1)
+              console.log('msg-----删除该 ' + n + ' 下指定方法')
+            }
+          })
+          this._events[n] = cbs
         }
-      })
-    }
+      } else {
+        console.log('msg----- ' + n + ' 未注册')
+      }
+    })
   }
 
   emit (name, ...args) {
-    if (!this._events[name]) {
-      console.log('msg-----未注册')
-      return false
+    if (!name || typeof name !== 'string') {
+      console.log('msg-----请输入正确的事件名称')
+      return
     }
-    if (args.length === 0) {
-      console.log('msg-----函数不存在或者已经被删除')
-      return false
-    }
-    this._events[name].forEach((cb) => cb(...args))
+
+    let names = name.split(' ')
+    names.forEach((n) => {
+      if (this._events[n]) {
+        let cbs = this._events[n]
+        cbs.forEach((h, idx) => {
+          if (h.ctx) {
+            h.cb.apply(h.ctx, ...args)
+          } else {
+            h.cb(...args)
+          }
+        })
+      } else {
+        console.log('msg----- ' + n + ' 未注册')
+      }
+    })
   }
 }
 
-const bus = new Event()
+function bindeEvent (self, name, cb, ctx, isOnce) {
+  if (!name || typeof name !== 'string' || typeof cb !== 'function') {
+    console.log('msg-----不合法')
+    return
+  }
+  if (!cb) {
+    console.log('msg-----未指定回调函数')
+    return
+  }
+  if (!cb.name) {
+    console.log('msg-----注意传入的回调函数为匿名函数，无法删除')
+  }
+  let names = name.split(' ')
+  names.forEach((n) => {
+    if (!self._events[n]) {
+      self._events[n] = []
+    }
+    self._events[n].push({
+      cb: cb,
+      ctx: ctx || '',
+      once: isOnce
+    })
+  })
+}
 
+const bus = new Event()
 let testFun = (val) => {
   console.log('test-----', val)
 }
-
 bus.on('test test2', testFun)
-bus.on('test', ()=>{
-  console.log('这是一个测试,匿名函数')
+bus.once('test', () => {
+  console.log('匿名函数的测试--test')
 })
-bus.emit('test', '这是一个测试')
-bus.emit('test2', '这是一个测试2')
-bus.off('test', testFun)
-bus.emit('test', '这是一个测试')
+bus.off('test77 test2', testFun)
+bus.emit('test test2', '这是一个test测试')
